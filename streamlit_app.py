79cb96bc8b9134fe8e0e4398567d31fed7f859ee
ischiore <123647402+ischiore@users.noreply.github.com>
2023-01-26 16:24:55 +0100

    breakfast menu aggiunto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit 
 
+streamlit.title('My parents First Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Spinach & Rocket Smoothie')
