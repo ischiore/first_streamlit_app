16d0b23e8ec2ac7de4c27d86fe7f9d5c9226cf88
ischiore <123647402+ischiore@users.noreply.github.com>
2023-01-26 16:53:50 +0100

    added breakfast favorites and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit 
 
-streamlit.title('My parents First Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Spinach & Rocket Smoothie')
+streamlit.title('My Parents First Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
